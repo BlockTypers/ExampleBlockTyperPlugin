41024ffad059c9ec1cf049487acfcda976019a5c
Anthony Ruffino <aruffino84@gmail.com>
2018-01-03 13:40:00 +0100

    shell scripts

--- pullMe.sh
+++ pullMe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-uuid=exampe-$(uuidgen)
+uuid=blocktyper-tmp-$(uuidgen)
 git clone https://github.com/spaarkimus/ExampleBlockTyperPlugin.git $uuid
 cd $uuid
 ./new.sh $1 $2
 mv target/projects/* ..
-rm -rf uuid
+rm -rf $uuid
